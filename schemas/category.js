ea9aa73c942ab66ca9f228d6961aeeed6456a4ee
quocdatend <hngdat2003@gmail.com>
2025-03-17 10:12:38 +0700

    first commit

---
+++ schemas/category.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose')
+let categorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },description:{
+        type:String,
+        default:"",
+    }
+},{
+    timestamps:true
+});
+module.exports = mongoose.model('category', categorySchema);
